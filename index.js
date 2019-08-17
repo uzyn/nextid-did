98f2b3c64c0579efc6c51ff08f4d6a0e0d84411f
U-Zyn Chua <chua@uzyn.com>
2019-08-17 15:18:51 +0800

    Bitcoin

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const assert = require('assert');
 const crypto = require('crypto');
 
+const ecdh = crypto.createECDH('secp256k1')
+
+function toBitcoin(nextPvt) {
+  console.log(ecdh.setPrivateKey(nextPvt));
+  console.log(ecdh.getPrivateKey());
+  console.log(ecdh.getPublicKey());
+  return {
+    public: '1xxxxxxx',
+    private: 'oaidjfoiajsdoifjadsf',
+  }
+}
+
 function main() {
-  const pvt = crypto.randomBytes(32); // 256-bit, same as Bitcoin
-  console.log(pvt);
+  const nextPvt = crypto.randomBytes(32); // 64 bytes => 512-bit
+  console.log(nextPvt);
+  console.log(toBitcoin(nextPvt));
 }
 
 main();
